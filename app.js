4c16ab154fe46e6a6237ee2d97ada95bd08ca833
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-02-26 15:22:48 -0300

    Updating the API structure with the express-generator

--- app.js
+++ app.js
@@ -1,69 +1,41 @@
-/**
- * Author: Jonathan Lincoln Brilhante
- * Email: jonathan.lincoln.brilhante@gmail.com
- *
- * Author: Wesnydy Lima Ribeiro
- * Email: wesnydy@lavid.ufpb.br
- */
-
-'use strict';
-
-/**
- * Module dependencies.
- */
+var createError = require('http-errors');
 var express = require('express');
-var bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var db = require('./config/db');
-// var settings = require('./config/settings');
-
-var bundle = require('./routes/bundle');
-var translate = require('./routes/translate');
-var video = require('./routes/video');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-/**
- * For parsing application/json and application/x-www-form-urlencoded
- */
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-/**
- * Public directory.
- */
-// app.use('/video', express.static(settings.contentsPath));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Allow cross origin requests.
- */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  next(createError(404));
 });
 
-/**
- * Register routes.
- */
-app.use('/video', video);
-app.use('/translate', translate);
-app.use('/', bundle);
-
-/**
- * Error handler.
- */
+// error handler
 app.use(function(err, req, res, next) {
-  if (app.get('env') !== 'development')
-    delete err.stack
-  res.status(err.status).json({
-    'error': {
-      'message': err.message,
-      'status': err.status,
-      'stack': err.stack
-    }
-  });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
