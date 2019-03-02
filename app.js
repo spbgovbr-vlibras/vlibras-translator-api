7d02aaa2b1bfeca346c7c24cb25ef566530722ea
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-03-02 17:35:31 -0300

    Added params validation

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import createError from 'http-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import validator from 'express-validator';
 
 import translatorRouter from './routes/textTranslator';
 
@@ -11,7 +10,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(validator());
 app.use(cookieParser());
 
 app.use('/', translatorRouter);
@@ -23,13 +21,13 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if(app.get('env') === 'development') {
+    console.error('\x1b[2m', err);
+    res.json({ error : err });
+  } else {
+    res.json({ error : err });
+  }
 });
 
 export default app;
--- controllers/textTranslator.js
+++ controllers/textTranslator.js
@@ -1,8 +1,21 @@
-
-// import 
+import createError from 'http-errors';
+import { validationResult } from 'express-validator/check';
 
 const translator = async function textTranslator(req, res, next) {
-  res.status(200).json(req.body.text)
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(createError(422, errors.array()));
+  }
+
+  try {
+    res.status(200).json(req.body.text);
+  }
+  catch (err) {
+    next(err);
+  }
+  
 }
 
 export default translator;
