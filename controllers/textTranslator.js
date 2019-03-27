e3813a8bf6bc7ed2e87d0c46e41e582419f5f078
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-03-27 11:06:11 -0300

    RPC implementation in textTranslator controller

--- controllers/textTranslator.js
+++ controllers/textTranslator.js
@@ -1,7 +1,7 @@
 import createError from 'http-errors';
 import uuid from 'uuid/v4';
 import { validationResult } from 'express-validator/check';
-import { sendToQueue } from '../helpers/queueWrapper';
+import setupConnection from '../helpers/queueConnection';
 import Translation from '../models/translation';
 
 const translator = async function textTranslator(req, res, next) {
@@ -14,9 +14,34 @@ const translator = async function textTranslator(req, res, next) {
 
 	try {
 		const uid = uuid();
+		const connectionChannel = await setupConnection();
 
-		await sendToQueue(process.env.TEXT_ROUTE, req.body.text, uid);
+		await connectionChannel.assertExchange(
+			process.env.EXCHANGE_NAME,
+			process.env.EXCHANGE_TYPE,
+			{ durable: true }
+		);
+
+		connectionChannel.consume(
+			process.env.CONSUMER_QUEUE,
+			(msg) => {
+				if (msg.properties.correlationId === uid) {
+					res.status(200).json(msg.content.toString());
+					setTimeout(() => { console.log("xxxx fechando canal xxxx"); connectionChannel.close(); }, 500);
+				}
+			},
+			{ noAck: true }
+		);
+
+		const payload = JSON.stringify({ text: req.body.text });
 		
+		await connectionChannel.publish(
+			process.env.EXCHANGE_NAME,
+			process.env.TEXT_ROUTE,
+			Buffer.from(payload),
+			{ correlationId: uid, replyTo: process.env.CONSUMER_QUEUE }
+		);
+
 		const translationRequest = new Translation({
 			text: req.body.text,
 			requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress
@@ -24,8 +49,6 @@ const translator = async function textTranslator(req, res, next) {
 
 		await translationRequest.save();
 
-		res.status(200).json(req.body.text);
-		
 	} catch (error) {
 		next(error);
 	}
