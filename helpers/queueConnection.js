eb1d1ec75aa9f7c60915f8d01af608ed4a163724
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-03-27 11:02:54 -0300

    Refactory queueWrapper to queueConnection

---
+++ helpers/queueConnection.js
@@ -0,0 +1,32 @@
+import amqplib from 'amqplib';
+
+let connection;
+
+const connectionURL = {
+	protocol: process.env.AMQP_PROTOCOL,
+	hostname: process.env.AMQP_HOST,
+	port: process.env.AMQP_PORT,
+	username: process.env.AMQP_USER,
+	password: process.env.AMQP_PASS
+};
+
+const setupConnection = async function setupQueueConnection() {
+	try {
+		if (connection === undefined) {
+			console.log(">>>> Criando nova conexão <<<<");
+			connection = await amqplib.connect(connectionURL);
+		}
+
+		console.log("#### Criando novo canal ####");
+		return await connection.createChannel();
+
+	} catch (error) {
+		if (connection !== undefined) {
+			setTimeout(() => {	connection.close();	}, 500);
+		}
+
+		throw new Error(error);
+	}
+}
+
+export default setupConnection;
