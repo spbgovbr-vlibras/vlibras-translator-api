66aab01b694fab08ce655c9a5b980ff0acb1ed0a
root <Jonathan Brilhante>
2019-03-28 17:53:08 -0300

    Removing old Files

--- routes/textTranslator.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { check } from 'express-validator/check';
-import translator from '../controllers/textTranslator';
-
-const translatorRouter = express.Router();
-
-translatorRouter.post('/translate', check('text').not().isEmpty(), translator);
-
-export default translatorRouter;
