fdc70c88cb18f31e2f4fd8c0a50c1d3d06dfa2b8
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-07-11 11:14:11 -0300

    Download route implementation

--- api/controllers/videoMaker.js
+++ api/controllers/videoMaker.js
@@ -3,7 +3,7 @@ import uuid from 'uuid/v4';
 import { validationResult } from 'express-validator/check';
 import setupConnection from '../helpers/queueConnection';
 import env from '../config/environments/environment';
-import { AVATARS, STATUS } from '../config/video/parameters';
+import { STATUS } from '../config/video/parameters';
 import Video from '../models/video';
 
 const videoMaker = async function librasVideoMaker(req, res, next) {
@@ -34,9 +34,7 @@ const videoMaker = async function librasVideoMaker(req, res, next) {
 			requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress
 		});
 
-		const videoParams = {
-			...(AVATARS.includes(req.query.avatar) && { avatar: req.query.avatar })
-		};
+		const videoParams = { avatar: req.body.avatar };
 
 		const payload = JSON.stringify({ gloss: req.body.gloss, options: videoParams });
 
@@ -82,7 +80,31 @@ const videoStatus = async function librasVideoGenerationStatus(req, res, next) {
 }
 
 const videoDownload = async function downloadLibrasvideo(req, res, next) {
-	next(createError(404));
+
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return next(createError(422, errors.array()));
+	}
+
+	try {
+			const query = { uid: req.params.requestUID };
+			const { status, path } = await Video.findOne(query).exec() || {};
+
+			if (status === undefined) {
+				return next(createError(404, 'There is no video with the received UID'));
+			}
+
+			if (status !== STATUS.generated || path === undefined) {
+				return next(createError(404, 'Video not generated'));
+			}
+
+			res.status(200).download(path, 'vlibrasvideo.mp4');
+
+	} catch (error) {
+		next(error);
+	}
+
 }
 
 export { videoMaker, videoStatus, videoDownload };
