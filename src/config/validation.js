4c38423b5ebffa3e64bec21900733921ad34d003
Wesnydy Ribeiro <wesnydy@gmail.com>
2019-10-20 14:16:06 -0300

    Fixing validation enum values

--- src/config/validation.js
+++ src/config/validation.js
@@ -1,16 +1,16 @@
 export const VALIDATION_VALUES = {
   textLength: { min: 1, max: 5000 },
-  ratingOptions: /^good$|^bad$/,
-  avatarOptions: /^icaro$|^hozana$/,
-  captionOptions: /^enable$|^disable$/,
+  ratingOptions: ['good', 'bad'],
+  avatarOptions: ['icaro', 'hozana'],
+  captionOptions: ['enable', 'disable'],
 };
 
 export const VALIDATION_ERROR = {
   textLength: `'text' exceeded ${VALIDATION_VALUES.textLength.max} characters limit.`,
   glossLength: `'gloss' exceeded ${VALIDATION_VALUES.textLength.max} characters limit.`,
   translationLength: `'translation' exceeded ${VALIDATION_VALUES.textLength.max} characters limit.`,
-  ratingOptions: `'rating' should match to ${VALIDATION_VALUES.ratingOptions}.`,
-  avatarOptions: `'avatar' should match to ${VALIDATION_VALUES.avatarOptions}.`,
-  captionOptions: `'caption' should match to ${VALIDATION_VALUES.captionOptions}.`,
+  ratingOptions: `'rating' is not in valid values ${VALIDATION_VALUES.ratingOptions}.`,
+  avatarOptions: `'avatar' is not in valid values ${VALIDATION_VALUES.avatarOptions}.`,
+  captionOptions: `'caption' is not in valid values ${VALIDATION_VALUES.captionOptions}.`,
   uuidVersion: '\'requestUID\' must be a UUID version 4.',
 };
