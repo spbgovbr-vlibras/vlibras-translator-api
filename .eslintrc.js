498b7d9aea86b2fb6bd0e4856a316f36d79a4205
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-02-07 18:14:56 -0300

    feat: git hooks with husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
-  extends: "airbnb-base",
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   rules: {
     "no-underscore-dangle": [
       "error",
