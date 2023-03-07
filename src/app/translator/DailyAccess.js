da18e86fb568015ed0ebf5201e03deb03ce3b610
joao.filho <joao.filho@lavid.ufpb.br>
2023-03-07 13:46:52 -0300

    feat: added schema for daily access

---
+++ src/app/translator/DailyAccess.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const options = {
+  timestamps: true,
+  versionKey: false,
+};
+
+const accessSchema = new Schema({
+  requestsAmount: {
+    type: Number,
+    required: true,
+  },
+  wordsAmount: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  site: {
+    type: String,
+    required: true,
+  },
+}, options);
+
+const AccessSchema = model('DailyAccess', accessSchema);
+
+export default AccessSchema;
