36c14af38b250552b251d85344842839cdc0737f
Joison <joison.pereira@lavid.ufpb.br>
2023-06-05 22:55:46 -0300

    feat(translationcache): improved asynchrony in cache access flow

--- src/app/middlewares/translationCache.js
+++ src/app/middlewares/translationCache.js
@@ -1,32 +1,41 @@
-import Translation from "../translator/Translation";
-import { cacheError } from "../util/debugger";
-import crypto from "crypto";
-import redisConnection from "../util/redisConnection";
+import crypto from 'crypto';
+import Translation from '../translator/Translation';
+import { cacheError } from '../util/debugger';
+import redisConnection from '../util/redisConnection';
+
+const countCachedTranslation = function (text, translation, requester) {
+  const translationRequest = new Translation({
+    text,
+    translation,
+    requester,
+  });
+
+  translationRequest.save();
+};
 
 const translationCache = async function getTranslationCache(req, res, next) {
   try {
     const redisClient = await redisConnection();
     const buffer = Buffer.from(
-      req.body.text.replace(/[^A-Z0-9]/gi, "").toLowerCase()
+      req.body.text.replace(/[^A-Z0-9]/gi, '').toLowerCase(),
     );
 
-    req.body.textHash = crypto.createHash("md5").update(buffer).digest("hex");
+    req.body.textHash = crypto.createHash('md5').update(buffer).digest('hex');
     const cachedTranslation = await redisClient.get(req.body.textHash);
 
     if (cachedTranslation === null) {
       return next();
-    } else {
-      const text = req.body.text;
-      const translation = cachedTranslation;
-      const requester =
-        req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-      try {
-        await countCachedTranslation(text, translation, requester);
-      } catch (error) {
-        cacheError(`COUNT ${error.message}`);
-      }
     }
+    const { text } = req.body;
+    const translation = cachedTranslation;
+    const requester = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    try {
+      countCachedTranslation(text, translation, requester);
+    } catch (error) {
+      cacheError(`COUNT ${error.message}`);
+    }
+
 
     return res.status(200).send(cachedTranslation);
   } catch (error) {
@@ -35,14 +44,5 @@ const translationCache = async function getTranslationCache(req, res, next) {
   }
 };
 
-const countCachedTranslation = async function (text, translation, requester) {
-  const translationRequest = new Translation({
-    text,
-    translation,
-    requester,
-  });
-
-  await translationRequest.save();
-};
 
 export default translationCache;
