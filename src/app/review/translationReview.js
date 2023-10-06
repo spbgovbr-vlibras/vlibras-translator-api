8713a721ce4f1db635d8e8862d06ba114b13c3ca
Marcos André <marcosandre@cc.ci.ufpb.br>
2023-10-06 16:04:25 -0300

    Migrating translation route

--- src/app/review/translationReview.js
+++ src/app/review/translationReview.js
@@ -1,16 +1,32 @@
-import Review from './Review';
+import { Review } from "../db/models"
 
 const translationReview = async function translationReviewController(req, res, next) {
   try {
-    const reviewRequest = new Review({
-      text: req.body.text,
-      translation: req.body.translation,
-      rating: req.body.rating,
+    // const reviewRequest = new Review({
+    //   text: req.body.text,
+    //   translation: req.body.translation,
+    //   rating: req.body.rating,
+    //   review: req.body.review || '',
+    //   requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    // });
+
+    // const review = new Review({
+    //   translationId: 1,
+    //   rating: false,
+    //   review: 'ruim',
+    //   requester: 'eu'
+    // });
+
+    const rating = req.body.rating === "good";
+
+    await Review.create({
+      translationId: req.body.translation,
+      rating: rating,
       review: req.body.review || '',
       requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     });
 
-    await reviewRequest.save();
+    // await Review.save();
     return res.sendStatus(200);
   } catch (error) {
     return next(error);
