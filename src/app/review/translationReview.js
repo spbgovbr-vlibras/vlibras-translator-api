efc193cc7733b316e31b60e4078f88416792fd0a
Lucas Bitu <lucasbitu@lavid.ufpb.br>
2023-10-18 17:00:35 -0300

    review-ok

--- src/app/review/translationReview.js
+++ src/app/review/translationReview.js
@@ -1,32 +1,18 @@
 import { Review } from "../db/models"
+import { Translation } from "../db/models"
 
 const translationReview = async function translationReviewController(req, res, next) {
   try {
-    // const reviewRequest = new Review({
-    //   text: req.body.text,
-    //   translation: req.body.translation,
-    //   rating: req.body.rating,
-    //   review: req.body.review || '',
-    //   requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-    // });
 
-    // const review = new Review({
-    //   translationId: 1,
-    //   rating: false,
-    //   review: 'ruim',
-    //   requester: 'eu'
-    // });
-
-    const rating = req.body.rating === "good";
-
-    await Review.create({
-      translationId: req.body.translation,
-      rating: rating,
+    const translation = await Translation.findOne( { where: {translation: req.body.translation} } );
+    const reviewRequest = new Review({
+      translationId: translation.id,
+      rating: req.body.rating === 'good',
       review: req.body.review || '',
       requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     });
 
-    // await Review.save();
+    await reviewRequest.save();
     return res.sendStatus(200);
   } catch (error) {
     return next(error);
