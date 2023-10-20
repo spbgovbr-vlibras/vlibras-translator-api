069137ba4e67b111fb9a55420d2510cc5417da89
Lucas Bitu <lucasbitu@lavid.ufpb.br>
2023-10-20 15:05:14 -0300

    transactions

--- src/app/review/translationReview.js
+++ src/app/review/translationReview.js
@@ -1,18 +1,23 @@
 import { Review } from "../db/models"
 import { Translation } from "../db/models"
+import db from '../db/models';
 
 const translationReview = async function translationReviewController(req, res, next) {
   try {
+    await db.sequelize.transaction(async (t) => {
+      const translation = await Translation.findOne({
+        where: { translation: req.body.translation }, 
+        transaction: t 
+      });
+      const reviewRequest = Review.build({
+        translationId: translation.id,
+        rating: req.body.rating === 'good',
+        review: req.body.review || '',
+        requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      });
 
-    const translation = await Translation.findOne( { where: {translation: req.body.translation} } );
-    const reviewRequest = new Review({
-      translationId: translation.id,
-      rating: req.body.rating === 'good',
-      review: req.body.review || '',
-      requester: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      await reviewRequest.save();
     });
-
-    await reviewRequest.save();
     return res.sendStatus(200);
   } catch (error) {
     return next(error);
